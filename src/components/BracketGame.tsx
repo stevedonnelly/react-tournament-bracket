4d440ac67fa88ac82ea1a44380598ce0583d84b0
Steve Donnelly <the.steve.donnelly@gmail.com>
2025-03-14 13:04:12 -0400

    test

--- src/components/BracketGame.tsx
+++ src/components/BracketGame.tsx
@@ -83,8 +83,8 @@ class BracketGame extends React.PureComponent<BracketGameProps> {
     const winnerBackground = (top && bottom && top.score && bottom.score && top.score.score !== bottom.score.score) ?
       (
         top.score.score > bottom.score.score ?
-          <rect x="170" y="12" width="1000" height="22.5" style={{ fill: winningScoreBackground }} rx="3" ry="3"/> :
-          <rect x="170" y="34.5" width="1000" height="22.5" style={{ fill: winningScoreBackground }} rx="3" ry="3"/>
+          <rect x="170" y="12" width="100" height="22.5" style={{ fill: winningScoreBackground }} rx="3" ry="3"/> :
+          <rect x="170" y="34.5" width="100" height="22.5" style={{ fill: winningScoreBackground }} rx="3" ry="3"/>
       ) :
       null;
 
@@ -124,7 +124,7 @@ class BracketGame extends React.PureComponent<BracketGameProps> {
       bottomHovered = (bottom && bottom.team && bottom.team.id === hoveredTeamId);
 
     return (
-      <svg width="200" height="82" viewBox="0 0 200 82" {...rest}>
+      <svg width="300" height="82" viewBox="0 0 300 82" {...rest}>
         {/* game time */}
         <text x="100" y="8" textAnchor="middle" style={gameTimeStyle}>
           {topText(game)}
